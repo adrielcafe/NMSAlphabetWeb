1655b6e2b716794d32cf4edbd04e5cfbb5df9824
Adriel Café <me@adriel.cafe>
2016-06-02 12:40:02 -0300

    CSS Nano

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 var slim = require("gulp-slim");
 var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
+var cssnano = require('gulp-cssnano');
 var rename = require('gulp-rename');
 
 var slimIndexFile = './index.slim';
@@ -11,7 +12,7 @@ var slimFiles = ['./*.slim', './slim/*.slim'];
 var sassFiles = './sass/*.sass';
 var jsFiles = ['./js/*.js', '!./js/*.min.js'];
 
-gulp.task('server', ['slim', 'sass', 'uglify', 'watch'], function() {
+gulp.task('server', ['slim', 'sass-cssnano', 'uglify', 'watch'], function() {
     browserSync.init({
         server: "./dist"
     });
@@ -23,9 +24,10 @@ gulp.task('slim', function(){
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('sass', function () {
+gulp.task('sass-cssnano', function () {
   return gulp.src(sassFiles)
-		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(sass().on('error', sass.logError))
+    .pipe(cssnano({discardComments: {removeAll: true}}))
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('./dist/css'))
     .pipe(browserSync.stream());
@@ -42,7 +44,7 @@ gulp.task('uglify', function () {
 gulp.task('watch', function () {
 	gulp.watch("./dist/*.html").on('change', browserSync.reload);
 	gulp.watch(slimFiles, ['slim']);
-  gulp.watch(sassFiles, ['sass']);
+  gulp.watch(sassFiles, ['sass-cssnano']);
 	gulp.watch(jsFiles, ['uglify']);
 });
 
