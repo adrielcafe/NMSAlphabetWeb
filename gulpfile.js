44bcefcc5fd73443371fa15fbaf9a8dc1023634c
Adriel Café <me@adriel.cafe>
2016-06-02 01:13:53 -0300

    Mega refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,49 @@
+
+var browserSync = require('browser-sync').create();
 var gulp = require('gulp');
-var express = require('express');
 var slim = require("gulp-slim");
 var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
 
 var slimIndexFile = './index.slim';
-var slimFiles = ['./*.slim', './html/*.slim'];
-var scssFiles = './css/*.sass';
+var slimFiles = ['./*.slim', './slim/*.slim'];
+var sassFiles = './sass/*.sass';
 var jsFiles = ['./js/*.js', '!./js/*.min.js'];
 
-gulp.task('server', function() {
-	var app = express();
-	app.use(express.static(__dirname));
-	app.listen(4000);
+gulp.task('server', ['slim', 'sass', 'uglify', 'watch'], function() {
+    browserSync.init({
+        server: "./dist"
+    });
 });
 
 gulp.task('slim', function(){
   gulp.src(slimIndexFile)
-    .pipe(slim({pretty: true}))
-    .pipe(gulp.dest('./'));
-});
-
-gulp.task('minify', function () {
-  return gulp.src(jsFiles)
-    .pipe(uglify())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('./js'));
+    .pipe(slim({pretty: false}))
+    .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('sass', function () {
-  return gulp.src(scssFiles)
+  return gulp.src(sassFiles)
 		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('uglify', function () {
+  return gulp.src(jsFiles)
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./dist/js'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function () {
+	gulp.watch("./dist/*.html").on('change', browserSync.reload);
 	gulp.watch(slimFiles, ['slim']);
-	gulp.watch(jsFiles, ['minify']);
-  gulp.watch(scssFiles, ['sass']);
+  gulp.watch(sassFiles, ['sass']);
+	gulp.watch(jsFiles, ['uglify']);
 });
 
-gulp.task('default', ['server', 'slim', 'minify', 'sass', 'watch']);
+gulp.task('default', ['server']);
